cde08ef04550a4957785feff474c6519dcebc2af
prabhatgrg <prabhatgurung34@gmail.com>
2022-03-12 07:58:44 +0545

    Updated timer to update dynamically hopefully works

--- js/script.js
+++ js/script.js
@@ -5,11 +5,12 @@ const secondsEl = document.getElementById('seconds');
 
 const birthday = '4 Jan 2023';
 
+const birthdayDate = new Date(birthday);
+const currentDate = new Date();
+
+
 function countdown()
 {
-    const birthdayDate = new Date(birthday);
-    const currentDate = new Date();
-
     const totalSeconds = (birthdayDate - currentDate)/1000;
 
     const days = Math.floor(totalSeconds / 3600 / 24);
@@ -17,16 +18,7 @@ function countdown()
     const mins = Math.floor(totalSeconds / 60) % 60;
     const seconds = Math.floor(totalSeconds) % 60;
     
-    // const year = birthdayDate.getFullYear();
-    // const newYear = currentDate.getFullYear();
-
-    // if(newYear>=year)
-    // {
-
-    // }
-
-
-
+    
     daysEl.innerHTML = days;
     hoursEl.innerHTML = formatTime(hours);
     minsEl.innerHTML = formatTime(mins);
@@ -43,3 +35,24 @@ function formatTime(time)
     return time < 10 ? (`0${time}`) : time;
     // If Time < 10 then return 0 with Time Else return Time
 }
+
+
+function checkYear()
+{
+    const year = birthdayDate.getFullYear();
+    const newYear = currentDate.getFullYear();
+
+    if(newYear>=year)
+    {
+        updatedYear = year + 1;
+        year = updatedYear;
+    }
+    else
+    {
+        console.log("No Update Needed!!!");
+    }
+}
+
+checkYear();
+
+setInterval(checkYear, 100000);
