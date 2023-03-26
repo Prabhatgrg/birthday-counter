cc1e4a81369747ead643c1c00b7fb72954e35d48
Prabhatgrg <prabhatgurung34@gmail.com>
2023-03-26 11:53:22 +0545

    Solved the problem where the timer was not visually changing

--- js/script.js
+++ js/script.js
@@ -5,48 +5,37 @@ const secondsElement = document.getElementById('seconds');
 
 const birthday = '4 Jan 2024'; // Place your DOB here
 
-const birthdayDate = new Date(birthday);
-const currentDate = new Date();
+function countdown()
+{
+    const birthdayDate = new Date(birthday);
+    const currentDate = new Date();
 
-
-function dateUpdater(){
-    const now = new Date();
-    if (now.getMonth() > birthdayDate.getMonth() || 
-        (now.getMonth() === birthdayDate.getMonth() && now.getDate() >= birthdayDate.getDate())) {
-      birthdayDate.setFullYear(now.getFullYear() + 1);
+    // Check if the birthday has already passed this year
+    if (currentDate.getMonth() > birthdayDate.getMonth() || 
+        (currentDate.getMonth() === birthdayDate.getMonth() && currentDate.getDate() >= birthdayDate.getDate())) {
+        birthdayDate.setFullYear(currentDate.getFullYear() + 1);
     }
     else {
         console.log("No Change Needed!!!");
     }
-}
-dateUpdater();
 
-function countdown()
-{
     const totalSeconds = (birthdayDate.getTime() - currentDate.getTime())/1000;
-
     const daysValue = Math.floor(totalSeconds / 3600 / 24);
     const hoursValue = Math.floor(totalSeconds / 3600) % 24;
     const minsValue = Math.floor(totalSeconds / 60) % 60;
     const secondsValue = Math.floor(totalSeconds) % 60;
     
-    // const daysValue = Math.floor(totalSeconds / 3600 / 24);
-    // const hoursValue = Math.floor((totalSeconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    // const minsValue = Math.floor((totalSeconds % (1000 * 60 * 60)) / (1000 * 60));
-    // const secondsValue = Math.floor((totalSeconds % (1000 * 60)) / 1000);
-
-    daysElement.innerHTML = daysValue;
-    hoursElement.innerHTML = formatTime(hoursValue);
-    minsElement.innerHTML = formatTime(minsValue);
-    secondsElement.innerHTML = formatTime(secondsValue);
-
+    daysElement.innerText = daysValue;
+    hoursElement.innerText = formatTime(hoursValue);
+    minsElement.innerText = formatTime(minsValue);
+    secondsElement.innerText = formatTime(secondsValue);
+    
 }
 
-setInterval(countdown, 1000);
 //Format Time:
-
 function formatTime(time)
 {
     return time < 10 ? (`0${time}`) : time;
     // If Time < 10 then return 0 with Time Else return Time
 }
+setInterval(countdown, 1000);
