d7ef856432e07b7cecc097dc2b0e8bc5b57b3b2d
Prabhatgrg <prabhatgurung34@gmail.com>
2023-03-26 11:12:25 +0545

    Made the timer dynamic

--- js/script.js
+++ js/script.js
@@ -1,58 +1,52 @@
-const daysEl = document.getElementById('days');
-const hoursEl = document.getElementById('hours');
-const minsEl = document.getElementById('mins');
-const secondsEl = document.getElementById('seconds');
+const daysElement = document.getElementById('days');
+const hoursElement = document.getElementById('hours');
+const minsElement = document.getElementById('mins');
+const secondsElement = document.getElementById('seconds');
 
-const birthday = '4 Jan 2023';
+const birthday = '4 Jan 2024'; // Place your DOB here
 
 const birthdayDate = new Date(birthday);
 const currentDate = new Date();
 
 
+function dateUpdater(){
+    const now = new Date();
+    if (now.getMonth() > birthdayDate.getMonth() || 
+        (now.getMonth() === birthdayDate.getMonth() && now.getDate() >= birthdayDate.getDate())) {
+      birthdayDate.setFullYear(now.getFullYear() + 1);
+    }
+    else {
+        console.log("No Change Needed!!!");
+    }
+}
+dateUpdater();
+
 function countdown()
 {
-    const totalSeconds = (birthdayDate - currentDate)/1000;
+    const totalSeconds = (birthdayDate.getTime() - currentDate.getTime())/1000;
 
-    const days = Math.floor(totalSeconds / 3600 / 24);
-    const hours = Math.floor(totalSeconds / 3600) % 24;
-    const mins = Math.floor(totalSeconds / 60) % 60;
-    const seconds = Math.floor(totalSeconds) % 60;
+    const daysValue = Math.floor(totalSeconds / 3600 / 24);
+    const hoursValue = Math.floor(totalSeconds / 3600) % 24;
+    const minsValue = Math.floor(totalSeconds / 60) % 60;
+    const secondsValue = Math.floor(totalSeconds) % 60;
     
-    
-    daysEl.innerHTML = days;
-    hoursEl.innerHTML = formatTime(hours);
-    minsEl.innerHTML = formatTime(mins);
-    secondsEl.innerHTML = formatTime(seconds);
+    // const daysValue = Math.floor(totalSeconds / 3600 / 24);
+    // const hoursValue = Math.floor((totalSeconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    // const minsValue = Math.floor((totalSeconds % (1000 * 60 * 60)) / (1000 * 60));
+    // const secondsValue = Math.floor((totalSeconds % (1000 * 60)) / 1000);
+
+    daysElement.innerHTML = daysValue;
+    hoursElement.innerHTML = formatTime(hoursValue);
+    minsElement.innerHTML = formatTime(minsValue);
+    secondsElement.innerHTML = formatTime(secondsValue);
 
 }
 
-countdown();
-
 setInterval(countdown, 1000);
+//Format Time:
 
 function formatTime(time)
 {
     return time < 10 ? (`0${time}`) : time;
     // If Time < 10 then return 0 with Time Else return Time
 }
-
-
-function checkYear()
-{
-    const year = birthdayDate.getFullYear();
-    const newYear = currentDate.getFullYear();
-
-    if(newYear>=year)
-    {
-        updatedYear = year + 1;
-        year = updatedYear;
-    }
-    else
-    {
-        console.log("No Update Needed!!!");
-    }
-}
-
-checkYear();
-
-setInterval(checkYear, 100000);
